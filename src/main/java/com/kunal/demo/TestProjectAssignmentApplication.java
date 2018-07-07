a0e2d3ee3041ebc23026647d6fcc076c3aecf539
kkunal2006 <kkunal2005@gmail.com>
2018-07-07 23:20:48 +0400

    Initial Push

---
+++ src/main/java/com/kunal/demo/TestProjectAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.kunal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectAssignmentApplication.class, args);
+	}
+}
